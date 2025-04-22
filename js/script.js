b3eb8ad59e63c79fdb3580f3d45b53ac0ac689b1
Daniele Fonseca <106083414+danisfon@users.noreply.github.com>
2025-04-22 11:43:42 -0300

    add alteração de idioma e modificando o style

--- js/script.js
+++ js/script.js
@@ -13,3 +13,28 @@ document.querySelectorAll('.secoes > div, .card, .habilidade').forEach(el => {
     observer.observe(el);
 });
 
+
+let idiomaAtual = "pt";
+
+function carregarIdioma(idioma) {
+    fetch(`json/${idioma}.json`)
+        .then(response => response.json())
+        .then(data => {
+            traduzirPagina(data);
+        });
+}
+
+function traduzirPagina(linguagem) {
+    document.querySelectorAll("[data-i18n]").forEach(elemento => {
+        const chave = elemento.getAttribute("data-i18n");
+        const valor = chave.split('.').reduce((obj, i) => obj[i], linguagem);
+        if (valor) {
+            elemento.textContent = valor;
+        }
+    });
+}
+
+function alterarIdioma() {
+    idiomaAtual = idiomaAtual === "pt" ? "en" : "pt";
+    carregarIdioma(idiomaAtual);
+}
